d433880c39b01b432e4b238bfde63abb65aaf110
Wattana <saeung2532@gmail.com>
2021-01-13 15:11:59 +0700

    test

--- src/App.js
+++ src/App.js
@@ -12,11 +12,82 @@ import {
 } from "react-router-dom";
 import Container from "@material-ui/core/Container";
 import Drawer from "./components/layouts/Drawer";
+import * as loginActions from "./actions/login.action";
+import LoginPage from "./components/pages/LoginPage/LoginPage";
+import HomePage from "./components/pages/HomePage/HomePage";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "center",
+    padding: theme.spacing(3),
+  },
+}));
 
 export default function App() {
+  const classes = useStyles();
+
+
+// Login Route
+const LoginRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      loginActions.isLoggedIn() ? (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      ) : (
+        <div className={classes.root}>
+          <Container className={classes.content} maxWidth={false}>
+            <Component {...props} />
+          </Container>
+        </div>
+      )
+    }
+  />
+);
+
+//Private Route
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      loginActions.isLoggedIn() ? (
+        <div className={classes.root}>
+          <Drawer company={loginActions.getTokenCompany()} />
+          <Container className={classes.content} maxWidth={false}>
+            <Component {...props} />
+          </Container>
+        </div>
+      ) : (
+        <Redirect
+          to={{ pathname: "/login", state: { from: props.location } }}
+        />
+      )
+    }
+  />
+);
+
   return (
-    <div>
-      <h1>AAAA</h1>
-    </div>
+    <Router
+      basename={
+        process.env.REACT_APP_IS_PRODUCTION === "1" ? "/monthlyplan" : ""
+      }
+    >
+      <Switch>
+        <LoginRoute exact path="/login" component={LoginPage} />
+        <PrivateRoute exact path="/" component={HomePage} />
+      </Switch>
+    </Router>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+import logger from "redux-logger";
+
+var middlewares = null;
+
+if (process.env.REACT_APP_IS_PRODUCTION === "1") {
+  console.log("REACT_APP_PRD");
+  middlewares = applyMiddleware(thunk);
+} else {
+  console.log("REACT_APP_TST");
+  middlewares = applyMiddleware(thunk, logger);
+}
+
+const store = createStore(reducers, middlewares);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
